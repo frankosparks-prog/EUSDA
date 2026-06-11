9124a6666ffb74820ec746b1fbdb91b0a07f2dbb
gitcat456 <okothdenzel65@gmail.com>
2026-06-11 15:36:20 +0300

    add trim and sparse options to email field

--- backend/models/Registration.js
+++ backend/models/Registration.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const registrationSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
+  phoneNumber: { type: String, required: true, unique: true, trim: true },
   gender: { type: String, enum: ["Male", "Female"], required: true },
-  email: { type: String, default: "" },
+  email: { type: String, default: null, unique: true, trim: true, sparse: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Registration", registrationSchema);
