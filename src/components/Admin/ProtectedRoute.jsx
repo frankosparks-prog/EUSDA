2d484042c44a534111a1dfe1a5e618102848db07
gitcat456 <okothdenzel65@gmail.com>
2026-08-31 20:49:38 +0300

    add ProtectedRoute for admin authentication

--- src/components/Admin/ProtectedRoute.jsx
+++ src/components/Admin/ProtectedRoute.jsx
@@ -3,14 +3,11 @@ import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem('adminToken');
-  console.log('adminToken:', token); // Check if token is retrieved
 
   if (!token) {
-    console.log('Redirecting to admin login');
     return <Navigate to="/admin" />;
   }
 
-  console.log('Access granted');
   return children;
 };
 
